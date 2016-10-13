b19ae8883797a4b8bb33206ef83e33a5633fc838
Michael Griffiths <mickg77@hotmail.com>
2016-10-13 11:28:41 +0000

    external toolbars

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!-- a document created by Michael Griffiths-->
+<html lang="en">
+    <head>
+        <title>Michael's Cake Shop </title>
+        <!--viewport sets up the site to fill the mobile screen-->
+        
+        
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+     
+              <!-- these two lines of code give us access to the jquery libraries-->
+       <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<script>
+$(function(){
+    $("[data-role='header'], [data-role='footer']").toolbar();
+});
+
+</script>
+
+
+
+<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
+<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/mystyles.css" type="text/css" />
+<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+    </head>
+    <body>
+       <div id="myhead" data-role="header" data-position="fixed" data-theme="a">
+             <h1>The Bakery</h1>
+             <a href="#" class="ui-btn-right">Nav</a>
+     </div>
+       
+       
+<!--the first page of the app-->       
+         
+        <section class="page" id="page1" data-role="page">
+            
+            </div>
+        <h2>About</h2>
+            
+            <p>Tootsie roll sugar plum muffin sesame snaps ice cream sugar plum topping jelly-o. 
+            Caramels I love I love apple pie dragée candy. Dessert biscuit gummies marshmallow. 
+            Jujubes fruitcake lemon drops oat cake. I love marshmallow toffee toffee donut. 
+            Fruitcake wafer candy canes gummi bears cookie cheesecake.</p>
+            
+        
+            <a href="#page2">gotopage2</a>
+            
+        </section>
+     
+<!--the second page of the app-->
+     
+      <section class="page" id="page2" data-role="page">
+          
+     <h2>Login</h2>
+            <p>Macaroon wafer halvah gingerbread. Sweet roll sweet biscuit macaroon brownie chocolate 
+            cake I love cupcake fruitcake. 
+            Candy I love ice cream cotton candy jujubes pastry fruitcake chupa chups. 
+            Marshmallow I love I love tiramisu gummi bears fruitcake. 
+            Jelly wafer tart croissant jelly cheesecake toffee. 
+            Candy canes cupcake jelly-o cookie halvah cookie. 
+            Sweet roll marshmallow donut dragée soufflé jelly beans. 
+            Tiramisu dragée I love jelly-o. 
+            Macaroon I love I love fruitcake cheesecake topping liquorice cheesecake chocolate. 
+            Liquorice apple pie pudding apple pie candy.
+            
+        </p>
+            
+            <a href="#page1">gotopage1</a>
+      
+      
+        </section>
+        <div data-role="footer" data-position="fixed" data-theme="a">This is the footer</div>
+<!--the third page of the app-->        
+
+        </body>
+        </html>
